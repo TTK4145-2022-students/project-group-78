1f54d091a436c957dbbf1342557f8e5d46a5f002
vetleras <vetleras@gmail.com>
2022-02-16 14:01:46 +0100

    Create peers module

---
+++ peers/peer.go
@@ -0,0 +1,69 @@
+package peers
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/TTK4145-2022-students/project-group-78/conn"
+	"github.com/TTK4145-2022-students/project-group-78/utils"
+)
+
+var TIMEOUT = time.Second
+var RESEND = 100 * time.Millisecond
+
+type Peer struct {
+	conn  *conn.Conn
+	outs  []chan []byte
+	times map[byte]time.Time
+	last  []byte
+	id    byte
+}
+
+func (p *Peer) New(id byte) *Peer {
+	localIp := net.ParseIP(fmt.Sprintf("127.0.0.%v", id))
+	port := 61523
+	return &Peer{
+		conn: conn.New(localIp, port, net.ParseIP("127.255.255.255"), port),
+		id:   id,
+	}
+
+}
+
+func (p *Peer) Subscribe(out chan []byte) {
+	p.outs = append(p.outs, out)
+}
+
+func (p *Peer) listen() {
+	select {
+	case b := <-p.conn.Receive:
+		id := b[0]
+		p.times[id] = time.Now()
+	}
+
+	peers := make([]byte, len(p.last))
+	for id, time_ := range p.times {
+		if time.Now().Sub(time_) < TIMEOUT {
+			peers = append(peers, id)
+		}
+	}
+	if utils.Equal(peers, p.last) {
+		for _, out := range p.outs {
+			out <- append([]byte{}, peers...) // Go's way of deep copy ...
+		}
+		p.last = peers
+	}
+}
+
+func (p *Peer) listenForever() {
+	for {
+		p.listen()
+	}
+}
+
+func (p *Peer) sendForever() {
+	for {
+		p.conn.Send([]byte{p.id})
+		time.Sleep(RESEND)
+	}
+}
--- utils/utils.go
+++ utils/utils.go
@@ -17,3 +17,15 @@ func Subset(subset []int, superset []int) bool {
 	}
 	return true
 }
+
+func Equal(s1 []byte, s2 []byte) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i := 0; i < len(s1); i++ {
+		if s1[i] != s2[i] {
+			return false
+		}
+	}
+	return true
+}

a313e35178752460914389a7480843c981018973
vetleras <vetleras@gmail.com>
2022-02-24 16:28:59 +0100

    Clean config

--- config/config.go
+++ config/config.go
@@ -1,17 +1,4 @@
 package config
 
-import (
-	"fmt"
-	"net"
-)
-
-var BROADCAST_IP = net.ParseIP("127.255.255.255")
-
-const PORT = 41784
-
 const NUM_ELEVATORS = 3
 const NUM_FLOORS = 4
-
-func LocalIp(id int) net.IP {
-	return net.ParseIP(fmt.Sprintf("127.0.0.%v", id))
-}

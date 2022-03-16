c294f96c39cdf462eb014af326b1ac4819f61bea
Magnus <magnuswii@gmail.com>
2022-03-16 15:02:12 +0100

    Add assigner timeouts

--- config/config.go
+++ config/config.go
@@ -2,8 +2,10 @@ package config
 
 import "time"
 
-const NUM_ELEVS = 3
+const NUM_ELEVS = 1
 const NUM_FLOORS = 4
 const DOOR_OPEN_TIME = 3 * time.Second
 const TRANSMIT_INTERVAL = 5 * time.Millisecond
 const CHAN_SIZE = 16
+const ORDER_TIMEOUT = time.Second
+const ELEV_TIMEOUT = 3 * time.Second

10498403c8fedfcad7a09542bfd5e76eaac2c3fd
vetleras <vetleras@gmail.com>
2022-03-18 10:25:16 +0100

    Fix door

--- door/door.go
+++ door/door.go
@@ -1,10 +1,19 @@
 package door
 
 import (
+	"log"
 	"time"
 
 	"github.com/TTK4145-2022-students/driver-go-group-78/elevio"
 	"github.com/TTK4145-2022-students/project-group-78/config"
+)
+
+type state int
+
+const (
+	closed state = iota
+	inCountdown
+	stuck
 )
 
 func Door(openC <-chan bool, closedC chan<- bool) {
@@ -12,30 +21,46 @@ func Door(openC <-chan bool, closedC chan<- bool) {
 	go elevio.PollObstructionSwitch(obstructionC)
 
 	obstructed := false
-	doorOpen := false
-
-	timer := time.NewTimer(time.Hour)
-	timer.Stop()
+	state := closed
+	var timer *time.Timer
 
 	for {
 		select {
 		case obstructed = <-obstructionC:
-			if !obstructed && doorOpen {
+			if state == stuck && !obstructed {
 				elevio.SetDoorOpenLamp(false)
-				doorOpen = false
 				closedC <- true
+				state = closed
 			}
 
 		case <-openC:
-			elevio.SetDoorOpenLamp(true)
-			doorOpen = true
-			timer = time.NewTimer(config.DoorOpenTime)
+			switch state {
+			case closed:
+				elevio.SetDoorOpenLamp(true)
+				timer = time.NewTimer(config.DoorOpenTime)
+				state = inCountdown
+
+			case inCountdown:
+				timer = time.NewTimer(config.DoorOpenTime)
+
+			case stuck:
+				timer = time.NewTimer(config.DoorOpenTime)
+				state = inCountdown
+
+			default:
+				log.Panicf("door: unknown state %v", state)
+			}
 
 		case <-timer.C:
-			if !obstructed && doorOpen {
+			if state != inCountdown {
+				log.Panicf("door: timer expired while in %v", state)
+			}
+			if obstructed {
+				state = stuck
+			} else {
 				elevio.SetDoorOpenLamp(false)
-				doorOpen = false
 				closedC <- true
+				state = closed
 			}
 		}
 	}
